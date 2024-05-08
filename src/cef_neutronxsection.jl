f8bf8d392afc1f944b48af26dd10bfc8af57ce30
abehersan <abehersan.77@gmail.com>
2024-05-08 11:32:33 +0200

    optimize neutron xsection
    the previous implementation allocated an incredibly large amount of memory and ran slowly (runtime > 1.5 s) even for moderate energy scans
    
    the current iteration runs in a couple of hundreds of microseconds and allocates less than 200 kb per iteration
    
    the API is the same

--- src/cef_neutronxsection.jl
+++ src/cef_neutronxsection.jl
@@ -8,89 +8,131 @@ function dipolar_formfactor(ion::mag_ion, Q::Real)::Float64
 end
 
 
-function calc_polmatrix!(pol_matrix::Matrix{Float64}, Q_cart::Vector{Float64})::Nothing
-    Q = normalize(Q_cart)
-    for I in CartesianIndices(pol_matrix)
+function calc_polmatrix(Qcart::Vector{Float64})::Matrix{Float64}
+    polmat = Matrix{Float64}(undef, (3, 3))
+    Q = normalize(Qcart)
+    for I in CartesianIndices(polmat)
         a, b = Tuple(I)
-        pol_matrix[I] = (isequal(a, b)*1.0 - Q[a]*Q[b])
+        polmat[I] = (isequal(a, b)*1.0 - Q[a]*Q[b])
     end
-    return nothing
+    return polmat
 end
 
 
-function calc_transitions!(calc_matrix, Ep::Vector{Float64}, Vp::Matrix{ComplexF64}, spin_ops)::Nothing
-    @inbounds for I in CartesianIndices(calc_matrix)
-        a, b = Tuple(I)
-        @inbounds for i in eachindex(Ep)
-            @inbounds for j in eachindex(Ep)
-                calc_matrix[I] += real(
-                        dot( Vp[:, i], spin_ops[a], Vp[:, j] ) *
-                        dot( Vp[:, j], spin_ops[b], Vp[:, i])
-                    )
+function calc_transitions(ion::mag_ion, i::Int64, Vp::Matrix{ComplexF64})::Vector{VEC{9}}
+    SIGMAS = VEC{9}[]
+    @views @inbounds for j in 1:size(Vp, 1)
+        sxx = real( dot( Vp[:, i], ion.Jx, Vp[:, j] ) * dot( Vp[:, j], ion.Jx, Vp[:, i] ) ) * (ion.g[1]*ion.g[1])
+        sxy = real( dot( Vp[:, i], ion.Jy, Vp[:, j] ) * dot( Vp[:, j], ion.Jx, Vp[:, i] ) ) * (ion.g[1]*ion.g[2])
+        sxz = real( dot( Vp[:, i], ion.Jz, Vp[:, j] ) * dot( Vp[:, j], ion.Jx, Vp[:, i] ) ) * (ion.g[1]*ion.g[3])
+
+        syx = real( dot( Vp[:, i], ion.Jx, Vp[:, j] ) * dot( Vp[:, j], ion.Jy, Vp[:, i] ) ) * (ion.g[2]*ion.g[1])
+        syy = real( dot( Vp[:, i], ion.Jy, Vp[:, j] ) * dot( Vp[:, j], ion.Jy, Vp[:, i] ) ) * (ion.g[2]*ion.g[2])
+        syz = real( dot( Vp[:, i], ion.Jz, Vp[:, j] ) * dot( Vp[:, j], ion.Jy, Vp[:, i] ) ) * (ion.g[2]*ion.g[3])
+
+        szx = real( dot( Vp[:, i], ion.Jx, Vp[:, j] ) * dot( Vp[:, j], ion.Jz, Vp[:, i] ) ) * (ion.g[3]*ion.g[1])
+        szy = real( dot( Vp[:, i], ion.Jy, Vp[:, j] ) * dot( Vp[:, j], ion.Jz, Vp[:, i] ) ) * (ion.g[3]*ion.g[2])
+        szz = real( dot( Vp[:, i], ion.Jz, Vp[:, j] ) * dot( Vp[:, j], ion.Jz, Vp[:, i] ) ) * (ion.g[3]*ion.g[3])
+        push!(SIGMAS, [sxx, sxy, sxz, syx, syy, syz, szx, szy, szz])
+    end
+    return SIGMAS
+end
+
+
+function calc_neutronspectrum_xtal(ion::mag_ion, Ep::Vector{Float64}, Vp::Matrix{ComplexF64}, Qcart::Vector{<:Real}, T::Real)::Vector{VEC{2}}
+    np = population_factor(Ep, T)
+    ffactor = dipolar_formfactor(ion, norm(Qcart))
+    polfactors = reshape(calc_polmatrix(Qcart), 9)
+    NXS = VEC{2}[]
+    @inbounds for i in eachindex(Ep)
+        if isapprox(np[i], 0.0, atol=PREC)
+            continue
+        end
+        sigmas = calc_transitions(ion, i, Vp)
+        @inbounds for j in eachindex(Ep)
+            dE = Ep[j] - Ep[i]
+            NINT = dot(polfactors, sigmas[j]) * np[i] * abs2(ffactor)
+            if dE < 0.0     # detailed balance
+                NINT *= exp( -abs(dE)/(kB*T) )
             end
+            push!(NXS, [dE, NINT])
         end
     end
-    return nothing
+    return NXS
 end
 
 
-function calc_neutronspectrum!(calc_arr, Q, Ep, Vp)
+function calc_neutronspectrum_powd(ion::mag_ion, Ep::Vector{Float64}, Vp::Matrix{ComplexF64}, Qlen::Real, T::Real)::Vector{VEC{2}}
+    np = population_factor(Ep, T)
+    ffactor = dipolar_formfactor(ion, Qlen)
+    NXS = VEC{2}[]
+    @inbounds for i in eachindex(Ep)
+        if isapprox(np[i], 0.0, atol=PREC)
+            continue
+        end
+        sigmas = calc_transitions(ion, i, Vp)
+        @inbounds for j in eachindex(Ep)
+            dE = Ep[j] - Ep[i]
+            NINT = sum(sigmas[j]) * np[i] * abs2(ffactor) * 2.0/3.0
+            if dE < 0.0     # detailed balance
+                NINT *= exp( -abs(dE)/(kB*T) )
+            end
+            push!(NXS, [dE, NINT])
+        end
+    end
+    return NXS
 end
 
 
-function get_QE(calc_df::DataFrame)::Matrix{Float64}
-    QE = zeros(nrow(calc_df), 4)
-    QE[:, 1] = calc_df.Qx
-    QE[:, 2] = calc_df.Qy
-    QE[:, 3] = calc_df.Qz
-    QE[:, 4] = calc_df.EN
-    return QE
+function simulate_Escan(NXS::Vector{VEC{2}}, Es::AbstractVector, R::Function=TAS_resfunc)
+    Is = zeros(length(Es))
+    @inbounds for i in eachindex(Es)
+        @inbounds for j in eachindex(NXS)
+            E, I = NXS[j]
+            Is[i] += I * R(Es[i], E)
+        end
+    end
+    return Is
 end
 
 
-function cef_neutronxsection_crystal!(single_ion::mag_ion, bfactors::DataFrame, calc_df::DataFrame; resfunc::Function=TAS_resfunc)::Nothing
-    ext_field = [mean(calc_df.Bx), mean(calc_df.By), mean(calc_df.Bz)]
-    E, V = eigen(cef_hamiltonian(single_ion, bfactors, B=ext_field))
+function cef_neutronxsection_crystal!(ion::mag_ion, cefparams::DataFrame, calc_df::DataFrame; resfunc::Function=TAS_resfunc)::Nothing
+    extfield = [mean(calc_df.Bx), mean(calc_df.By), mean(calc_df.Bz)]
+    E, V = eigen(cef_hamiltonian(ion, cefparams, B=extfield))
     E .-= minimum(E)
     T = mean(calc_df.T)
-    np = population_factor(E, T)
+    Q = [mean(calc_df.Qx), mean(calc_df.Qy), mean(calc_df.Qz)]
+    NINT = calc_neutronspectrum_xtal(ion, E, V, Q, T)
+    EN = calc_df.EN
+    II = simulate_Escan(NINT, EN, resfunc)
+    calc_df[!, :I_CALC] = II
     return nothing
 end
 
 
-function cef_neutronxsection_powder!(single_ion::mag_ion, bfactors::DataFrame,
-                                    calc_df::DataFrame; resfunc::Function=TAS_resfunc)::Nothing
-    E, V = eigen(cef_hamiltonian(single_ion, bfactors))
+function cef_neutronxsection_powder!(ion::mag_ion, cefparams::DataFrame, calc_df::DataFrame; resfunc::Function=TAS_resfunc)::Nothing
+    E, V = eigen(cef_hamiltonian(ion, cefparams))
     E .-= minimum(E)
-    spin_ops = [spin_operators(single_ion.J, "x"),
-                spin_operators(single_ion.J, "y"),
-                spin_operators(single_ion.J, "z")]
-
-    @eachrow! calc_df begin
-        @newcol :I_CALC::Vector{Float64}
-        ffactor_term = abs(dipolar_formfactor(single_ion, :Q))^2
-        S_alphabeta = Matrix{Float64}(undef, (3, 3))
-        for I in CartesianIndices(S_alphabeta)
-            a, b = Tuple(I)
-            S_alphabeta[I] = calc_Salphabeta(:EN, :T, Ep=E, Vp=V, R=resfunc, J_alpha=spin_ops[a], J_beta=spin_ops[b])
-        end
-        :I_CALC = ffactor_term * muB^2 * norm(single_ion.g)^2 * 2.0/3.0 * sum(S_alphabeta)
-    end
-    return
+    T = mean(calc_df.T)
+    Q = mean(calc_df.Q)
+    NINT = calc_neutronspectrum_powd(ion, E, V, Q, T)
+    EN = calc_df.EN
+    II = simulate_Escan(NINT, EN, resfunc)
+    calc_df[!, :I_CALC] = II
+    return nothing
 end
 
 
-function TAS_resfunc(E::Float64, Epeak::Float64,
-                    width::Function=x->0.03*x+0.09)::Float64
-    gaussian(x=E, center=Epeak, amplitude=1.0, sigma=width(E))
+function TAS_resfunc(E::Float64, Epeak::Float64, width::Function=x->0.5)::Float64
+    return gaussian(x=E, center=Epeak, amplitude=1.0, sigma=width(E))
 end
 
 
 function gaussian(; x::Real, amplitude::Real, center::Real, sigma::Real)::Float64
-    amplitude * exp(-(x-center)^2 / (2*sigma^2)) / (sqrt(2pi) * sigma)
+    return amplitude * exp(-(x-center)^2 / (2*sigma^2)) / (sqrt(2pi) * sigma)
 end
 
 
 function lorentz(; x::Real, amplitude::Real, center::Real, sigma::Real)::Float64
-    amplitude / pi * (sigma / ( (x-center)^2 + sigma^2 ) )
+    return amplitude / pi * (sigma / ( (x-center)^2 + sigma^2 ) )
 end
